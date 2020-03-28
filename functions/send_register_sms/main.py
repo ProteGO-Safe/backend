dd3fab427e04007a329f54ceaec928c2313c5d7e
Karol Kostrzewa <karol.kostrzewa@polidea.com>
2020-03-28 19:26:15 +0100

    fix env

--- functions/send_register_sms/main.py
+++ functions/send_register_sms/main.py
@@ -5,28 +5,28 @@ import logging
 from smsapi.client import SmsApiPlClient
 from smsapi.exception import SmsApiException
 
-token = os.getenv('SMS_API_TOKEN')
+token = os.environ["SMS_API_TOKEN"]
 
 client = SmsApiPlClient(access_token=token)
 
 
 def send_register_sms(event, context):
-    if 'data' not in event:
-        logging.warning('send_register_sms: no data received')
+    if "data" not in event:
+        logging.warning("send_register_sms: no data received")
         return
 
     try:
-        data = json.loads(base64.b64decode(event['data']).decode('utf-8'))
+        data = json.loads(base64.b64decode(event["data"]).decode("utf-8"))
     except Exception as e:
-        logging.exception(f'send_register_sms: exception parsing data: {e}')
+        logging.exception(f"send_register_sms: exception parsing data: {e}")
         return
 
-    if 'phone_no' not in data or 'code' not in data:
-        logging.warning('send_register_sms: got incomplete data')
+    if "phone_no" not in data or "code" not in data:
+        logging.warning("send_register_sms: got incomplete data")
         return
 
-    phone_no = data['phone_no']
-    code = data['code']
+    phone_no = data["phone_no"]
+    code = data["code"]
     message = f"Kod weryfikacyjny do aplikacji Anna: {code}"
 
     try:
