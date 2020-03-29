48974d870c6fc84bece0b5c0d76e059910f579f6
Dariusz Aniszewski <dariusz.aniszewski@polidea.com>
2020-03-29 10:43:34 +0200

    refactor + documentation

--- functions/get_status/main.py
+++ functions/get_status/main.py
@@ -1,16 +1,17 @@
+import logging
 import os
 import uuid
+from datetime import datetime, timedelta
 from typing import Optional
 
 import pytz
-import logging
-from datetime import datetime, timedelta
 from flask import jsonify
-
 from google.cloud import bigquery, datastore
 from google.cloud.datastore import Entity
 
 NR_BEACON_IDS = 24 * 7
+
+BQ_TABLE = os.environ["BQ_TABLE"]
 
 datastore_client = datastore.Client()
 
@@ -32,7 +33,7 @@ def get_status(request):
 
     request_data = request.get_json()
 
-    for key in ["registration_id", "platform", "os_version", "device_name", "app_version", "lang"]:
+    for key in ["user_id", "platform", "os_version", "device_name", "app_version", "lang"]:
         if key not in request_data:
             return jsonify(
                 {
@@ -41,9 +42,14 @@ def get_status(request):
                 }
             ), 400
 
-    registration_id = request_data["registration_id"]
+    user_id = request_data["user_id"]
+    platform = request_data["platform"]
+    os_version = request_data["os_version"]
+    device_name = request_data["device_name"]
+    app_version = request_data["app_version"]
+    lang = request_data["lang"]
 
-    entity = _get_from_datastore(registration_id)
+    entity = _get_entity_from_datastore(user_id)
     if not entity:
         return jsonify(
             {
@@ -55,9 +61,9 @@ def get_status(request):
     beacons = _generate_beacons()
     try:
         # This must be synchronous call - successful saving beacon_ids to BigQuery is essential
-        _save_beacons(registration_id, beacons)
+        _save_beacons_to_bigquery(user_id, beacons)
     except SaveToBigQueryFailedException as error:
-        logging.error(f'Unable to save beacon_ids for registration_id: {registration_id}')
+        logging.error(f'Unable to save beacon_ids for user_id: {user_id}')
         for e in error.bq_errors:
             logging.error(e)
         return jsonify(
@@ -66,7 +72,7 @@ def get_status(request):
              }
         ), 500
 
-    _update_entity(entity, request_data)
+    _update_entity(entity, platform, os_version, app_version, device_name, lang)
     return jsonify(
         {
             "status": "orange",
@@ -78,23 +84,26 @@ def get_status(request):
     )
 
 
-def _get_from_datastore(registration_id: str) -> Optional[Entity]:
+def _get_entity_from_datastore(user_id: str) -> Optional[Entity]:
     kind = "Device"
     query = datastore_client.query(kind=kind)
-    query.add_filter('registration_id', '=', registration_id)
+    query.add_filter('user_id', '=', user_id)
     try:
         return list(query.fetch())[0]
     except IndexError:
         return None
 
 
-def _update_entity(entity: Entity, data: dict) -> None:
-    entity['platform'] = data['platform']
-    entity['os_version'] = data['os_version']
-    entity['device_name'] = data['device_name']
-    entity['app_version'] = data['app_version']
-    entity['lang'] = data['lang']
-    entity['last_status_requested'] = datetime.utcnow()
+def _update_entity(entity: Entity, platform: str, os_version: str, app_version: str, device_name: str,
+                   lang: str) -> None:
+    entity.update({
+        "platform": platform,
+        "os_version": os_version,
+        "app_version": app_version,
+        "device_name": device_name,
+        "lang": lang,
+        "last_status_requested": datetime.utcnow(),
+    })
     datastore_client.put(entity)
 
 
@@ -112,9 +121,9 @@ def _get_beacon_date(delta: timedelta = None) -> datetime:
     return datetime.utcnow().replace(tzinfo=pytz.utc, minute=0, second=0, microsecond=0) + delta
 
 
-def _save_beacons(registration_id, beacons):
+def _save_beacons_to_bigquery(registration_id, beacons):
     client = bigquery.Client()
-    table = client.get_table(os.getenv("BQ_TABLE"))
+    table = client.get_table(BQ_TABLE)
 
     rows_to_insert = [(registration_id, beacon["beacon_id"], beacon["date"]) for beacon in beacons]
 
