117454bf8aaeda8f686b6de1cc4c59157dca0daf
Dariusz Aniszewski <dariusz@aniszewski.eu>
2020-03-29 18:12:45 +0200

    get_status zapisuje dane o użytkowniku; zapis `beacon_id` w BigQuery (#16)
     /get_status zapisuje dane o użytkowniku + zapis `beacon_id` w BigQuery

--- functions/get_status/README.md
+++ functions/get_status/README.md
@@ -12,19 +12,26 @@ URL=https://${REGION}-${PROJECT_ID}.cloudfunctions.net/get_status
 
 #### Request details:
 ```
-method: GET
-```
+method: POST
+content-type: Application/json
+params:
+user_id
+platform
+os_version
+device_name
+app_version
+lang
 
-```
-curl ${URL}
+curl -X POST ${URL} -d '{"user_id": "UUID", "platform": "PLATFORM", "os_version": 0.0, "device_name": "NAME", "app_version": 0.0, "lang": "PL"}'
 {
+    "status":"orange"
     "beacon_ids": [
         {
            "beacon_id": "8d672642-7046-11ea-8e40-3f45fb5912b4", 
             "date":"2020032716"
-        }
+        },
+        [...]
     ],
-    "status":"orange"
 }
 ```
 
@@ -34,13 +41,15 @@ Required variables to deploy function:
 * `PROJECT_ID` - GCP project ID
 * `REGION` - region of the GCP project
 * `FUNCTIONS_BUCKET` - bucket where functions are stored
-
+* `BQ_TABLE` - full path to BigQuery table in form `<PROJECT>.<DATASET>.<TABLE_NAME>`
 
 Example deploy function (you have to be authorized to gcloud):
 ```bash
 REGION=europe-west3 \
 PROJECT_ID=<project_id> \
 FUNCTIONS_BUCKET=${PROJECT_ID}-functions \
+BQ_DATASET=<dataset-name> \
+BQ_TABLE=<table-name> \
 ./deploy.sh
 ```
 
@@ -51,5 +60,6 @@ gcloud functions deploy get_status \
     --source=./ \
     --runtime=python37 \
     --stage-bucket=${FUNCTIONS_BUCKET} \
+    --set-env-vars BQ_DATASET=${BQ_DATASET},BQ_TABLE=${BQ_TABLE} \
     --trigger-http --allow-unauthenticated
 ```
--- functions/get_status/main.py
+++ functions/get_status/main.py
@@ -1,26 +1,132 @@
-from datetime import datetime, timedelta
-
-from flask import jsonify
-import pytz
+import logging
+import os
 import uuid
+from datetime import datetime, timedelta
+from typing import Optional
 
+import pytz
+from flask import jsonify
+from google.cloud import bigquery, datastore
+from google.cloud.datastore import Entity
 
 NR_BEACON_IDS = 24 * 7
 
+BQ_TABLE_ID = f"{os.environ['GCP_PROJECT']}.{os.environ['BQ_DATASET']}.{os.environ['BQ_TABLE']}"
+
+datastore_client = datastore.Client()
+
+
+class SaveToBigQueryFailedException(Exception):
+    bq_errors = None
+
+    def __init__(self, bq_errors):
+        self.bq_errors = bq_errors
+
 
 def get_status(request):
+    if not request.is_json:
+        return jsonify(
+            {'status': 'failed',
+             'message': 'invalid data'
+             }
+        ), 422
+
+    request_data = request.get_json()
+
+    for key in ["user_id", "platform", "os_version", "device_type", "app_version", "lang"]:
+        if key not in request_data:
+            return jsonify(
+                {
+                    'status': 'failed',
+                    'message': f'missing field: {key}',
+                }
+            ), 422
+
+    user_id = request_data["user_id"]
+    platform = request_data["platform"]
+    os_version = request_data["os_version"]
+    device_type = request_data["device_type"]
+    app_version = request_data["app_version"]
+    lang = request_data["lang"]
+
+    entity = _get_entity_from_datastore(user_id)
+    if not entity:
+        return jsonify(
+            {
+                'status': 'failed',
+                'message': f'unauthorized',
+            }
+        ), 401
+
+    beacons = _generate_beacons()
+    try:
+        # This must be synchronous call - successful saving beacon_ids to BigQuery is essential
+        _save_beacons_to_bigquery(user_id, beacons)
+    except SaveToBigQueryFailedException as error:
+        logging.error(f'Unable to save beacon_ids for user_id: {user_id}')
+        for e in error.bq_errors:
+            logging.error(e)
+        return jsonify(
+            {'status': 'failed',
+             'message': 'internal exception'
+             }
+        ), 500
+
+    _update_entity(entity, platform, os_version, app_version, device_type, lang)
     return jsonify(
         {
             "status": "orange",
-            "beacon_ids": [
-                {"date": _now(timedelta(hours=i)), "beacon_id": uuid.uuid4()}
-                for i in range(0, NR_BEACON_IDS)
-            ],
+            "beacon_ids": [{
+                "date": beacon["date"].strftime("%Y%m%d%H%M%S"),
+                "beacon_id": beacon["beacon_id"],
+            } for beacon in beacons],
         }
     )
 
 
-def _now(delta: timedelta = None) -> str:
+def _get_entity_from_datastore(user_id: str) -> Optional[Entity]:
+    kind = "Device"
+    query = datastore_client.query(kind=kind)
+    query.add_filter('user_id', '=', user_id)
+    try:
+        return list(query.fetch())[0]
+    except IndexError:
+        return None
+
+
+def _update_entity(entity: Entity, platform: str, os_version: str, app_version: str, device_type: str,
+                   lang: str) -> None:
+    entity.update({
+        "platform": platform,
+        "os_version": os_version,
+        "app_version": app_version,
+        "device_type": device_type,
+        "lang": lang,
+        "last_status_requested": datetime.utcnow(),
+    })
+    datastore_client.put(entity)
+
+
+def _generate_beacons():
+    return [
+        {
+            "date": _get_beacon_date(timedelta(hours=i)),
+            "beacon_id": str(uuid.uuid4())
+        } for i in range(0, NR_BEACON_IDS)
+    ]
+
+
+def _get_beacon_date(delta: timedelta = None) -> datetime:
     delta = delta if delta is not None else timedelta()
-    now = datetime.utcnow().replace(tzinfo=pytz.utc) + delta
-    return now.strftime("%Y%m%d%H")
+    return datetime.utcnow().replace(tzinfo=pytz.utc, minute=0, second=0, microsecond=0) + delta
+
+
+def _save_beacons_to_bigquery(user_id, beacons):
+    client = bigquery.Client()
+    table = client.get_table(BQ_TABLE_ID)
+
+    rows_to_insert = [(user_id, beacon["beacon_id"], beacon["date"]) for beacon in beacons]
+
+    errors = client.insert_rows(table, rows_to_insert)
+    if errors:
+        raise SaveToBigQueryFailedException(errors)
