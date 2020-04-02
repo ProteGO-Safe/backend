6284894b70f467746bd95124b99ce744b3b4f1a5
Dariusz Aniszewski <dariusz.aniszewski@polidea.com>
2020-04-02 23:44:08 +0200

    poprawka czytania `last_beacon_date`

--- functions/get_status/main.py
+++ functions/get_status/main.py
@@ -47,7 +47,7 @@ def get_status(request):
     device_type = request_data["device_type"]
     app_version = request_data["app_version"]
     lang = request_data["lang"]
-    last_beacon_date = request_data.get["last_beacon_date"]
+    last_beacon_date = request_data["last_beacon_date"]
 
     user_entity = _get_user_entity(user_id)
     if not user_entity:
