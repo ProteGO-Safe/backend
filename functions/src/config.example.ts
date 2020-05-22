ae5edb38d1f8fbee5c7d271677ce063d828ce534
artur.kowalski <artur.kowalski@thecoders.io>
2020-05-22 21:40:46 +0000

    PSAFE-980 Add additional security for code generating

--- functions/src/config.example.ts
+++ functions/src/config.example.ts
@@ -2,6 +2,7 @@ import RandomCodeGenerator from "./utils/RandomCodeGenerator";
 import Config from "./interfaces/Config"
 import CodeRepository from "./utils/CodeRepository";
 import SecretManager from "./utils/SecretManager";
+import IPChecker from "./utils/IPChecker";
 
 const config: Config = {
     secretManagerPath: "/path/to/your/secret/object/versions/latest",
@@ -18,11 +19,13 @@ const config: Config = {
     jwt: {
         lifetime: 30
     },
-    secretManager: new SecretManager(),
     cache: {
         maxAge: 1800,
         sMaxAge: 1800
     }
 }
+
+export const secretManager = new SecretManager();
+export const ipChecker = new IPChecker();
 
 export default config;
--- functions/src/functions/generateCode.ts
+++ functions/src/functions/generateCode.ts
@@ -1,11 +1,15 @@
-import config from "../config";
+import config, {ipChecker, secretManager} from "../config";
 import {CallableContext} from "firebase-functions/lib/providers/https";
 import * as functions from "firebase-functions";
 import moment = require("moment");
 
 export async function generateCode(data : any, context: CallableContext) {
-    if (context.rawRequest.header('api-token') !== await config.secretManager.getConfig('apiToken')) {
-        throw new functions.https.HttpsError('unauthenticated', 'Invalid api token');
+    if (!await ipChecker.allow(context.rawRequest.ip)) {
+        throw new functions.https.HttpsError('permission-denied', 'Permission denied.');
+    }
+
+    if (context.rawRequest.header('api-token') !== await secretManager.getConfig('apiToken')) {
+        throw new functions.https.HttpsError('permission-denied', 'Permission denied.');
     }
 
     const code = config.code.generator.generate();
