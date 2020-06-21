d321bb36edd1b1c815fe9789dc8b6d67575c3ed8
thecodersio <artur.kowalski@thecoders.io>
2020-06-21 15:53:19 +0200

    PSAFE-1296 Change logic of generating pin code (#32)

--- functions/src/utils/RandomCodeGenerator.ts
+++ functions/src/utils/RandomCodeGenerator.ts
@@ -9,7 +9,34 @@ class RandomCodeGenerator {
     }
 
     generate(): string {
-        return cryptoRandomString({length: this.length}).toUpperCase();
+        let pin;
+
+        do {
+            pin = cryptoRandomString({length: this.length}).toUpperCase();
+        } while (!RandomCodeGenerator.isAlphaNumeric(pin))
+
+        return pin;
+    }
+
+    private static isAlphaNumeric(code: string): boolean {
+        let i, charCode, isAlpha = false, isNumeric = false;
+
+        for (i = 0, code.length; i < code.length; i++) {
+            charCode = code.charCodeAt(i);
+
+            switch (true){
+                case code > 47 && code < 58:
+                    isNumeric = true;
+                    break;
+                case (code > 64 && code < 91):
+                    isAlpha = true;
+                    break;
+                default:
+                    return false;
+            }
+        }
+
+        return isAlpha && isNumeric;
     }
 }
 
