92b5d69461d51ccad126f334b6468537bd7456be
Tomasz Molenda <tomasz.molenda@gmail.com>
2020-10-07 22:00:02 +0200

    PSAFE-2300 - test spring boot

---
+++ test/src/main/java/pl/gov/mc/protegosafe/efgs/CloudFunctionMain.java
@@ -0,0 +1,23 @@
+package pl.gov.mc.protegosafe.efgs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class CloudFunctionMain {
+
+	@Autowired TestService testService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFunctionMain.class, args);
+	}
+
+	@Bean
+	public Function<String, String> uppercase() {
+		return value -> testService.toUpperCase(value);
+	}
+}
---
+++ test/src/main/java/pl/gov/mc/protegosafe/efgs/TestService.java
@@ -0,0 +1,11 @@
+package pl.gov.mc.protegosafe.efgs;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    String toUpperCase(String value) {
+        return "dupa";
+    }
+}
