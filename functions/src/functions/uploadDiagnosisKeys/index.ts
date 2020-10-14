4d26d8dec5f6b19c0c7eb59036b38dfd81dac8ed
Tomasz Molenda <tomasz.molenda@gmail.com>
2020-10-14 16:48:09 +0200

    PSAFE-2300 - refactor gens uploader

--- functions/src/functions/uploadDiagnosisKeys/index.ts
+++ functions/src/functions/uploadDiagnosisKeys/index.ts
@@ -6,35 +6,35 @@ const superagent = require('superagent');
 
 const uploadDiagnosisKeys = (data: any): Promise<any> => {
     const idToken = getIdToken();
+    console.log(idToken);
     return superagent
         .post(config.exposureEndpoint)
         .send(data)
-        .set('Authorization', `Bearer ${idToken}`);
+        .set('Authorization', `Bearer ${idToken}`)
+};
 
+const getIdToken = async (): Promise<string> => {
+    const serverConfig = await secretManager.getConfig('exposureServerConfig');
 
-    async function getIdToken(): Promise<string> {
-        const serverConfig = await secretManager.getConfig('exposureServerConfig');
+    const jwt = sign(
+        {
+            target_audience: config.exposureEndpoint
+        },
+        <string>serverConfig.private_key,
+        {
+            algorithm: 'RS256',
+            expiresIn: '60 minutes',
+            audience: serverConfig.token_uri,
+            issuer: serverConfig.client_email
+        }
+    );
 
-        const jwt = sign(
-            {
-                target_audience: config.exposureEndpoint
-            },
-            <string>serverConfig.private_key,
-            {
-                algorithm: 'RS256',
-                expiresIn: '60 minutes',
-                audience: serverConfig.token_uri,
-                issuer: serverConfig.client_email
-            }
-        );
+    const response = await Axios.post(serverConfig.token_uri, {
+        grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+        assertion: jwt
+    });
 
-        const response = await Axios.post(serverConfig.token_uri, {
-            grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-            assertion: jwt
-        });
-
-        return <string>response.data.id_token;
-    }
+    return <string>response.data.id_token;
 }
 
 export default uploadDiagnosisKeys;
