7f52db62b2b5cfbec7667003d519ba318ae042be
Tomasz Molenda <tomasz.molenda@gmail.com>
2020-10-14 16:10:13 +0200

    PSAFE-2300 - restored node uploader keys to gens

--- functions/src/functions/uploadDiagnosisKeysHttpHandler/index.ts
+++ functions/src/functions/uploadDiagnosisKeysHttpHandler/index.ts
@@ -1,20 +1,24 @@
-import {verify} from "jsonwebtoken";
+import {sign, verify} from "jsonwebtoken";
 import config, {secretManager} from "../../config";
 import * as functions from "firebase-functions";
-
-const {log} = require("firebase-functions/lib/logger");
+const { log } = require("firebase-functions/lib/logger");
+const superagent = require('superagent');
+import Axios from "axios";
 import {v4} from "uuid";
 import * as admin from "firebase-admin";
-import uploadDiagnosisKeys from "../uploadDiagnosisKeys";
 
-export async function uploadDiagnosisKeysHttpHandler(request: functions.Request, response: functions.Response) {
+export async function uploadDiagnosisKeys(request : functions.Request, response : functions.Response) {
     const body = request.body;
     if (!await auth(body.data.verificationPayload)) {
         return response.status(401).send({error: {message: "", status: "UNAUTHENTICATED"}});
     }
+    const idToken = await getIdToken();
 
     try {
-        await uploadDiagnosisKeys(body.data)
+        await superagent
+            .post(config.exposureEndpoint)
+            .send(body.data)
+            .set('Authorization', `Bearer ${idToken}`)
             .then((ignore: any) => saveDiagnosisKeys(body))
     } catch (error) {
         if (error.response && error.response.error) {
@@ -22,7 +26,7 @@ export async function uploadDiagnosisKeysHttpHandler(request: functions.Request,
         }
     }
 
-    return response.status(200).send({result: ""});
+    return response.status(200).send({result:""});
 }
 
 async function auth(token: string | undefined): Promise<boolean> {
@@ -39,6 +43,30 @@ async function auth(token: string | undefined): Promise<boolean> {
     return true;
 }
 
+async function getIdToken(): Promise<string> {
+    const serverConfig = await secretManager.getConfig('exposureServerConfig');
+
+    const jwt = sign(
+        {
+            target_audience: config.exposureEndpoint
+        },
+        <string> serverConfig.private_key,
+        {
+            algorithm: 'RS256',
+            expiresIn: '60 minutes',
+            audience: serverConfig.token_uri,
+            issuer: serverConfig.client_email
+        }
+    );
+
+    const response = await Axios.post(serverConfig.token_uri, {
+        grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+        assertion: jwt
+    });
+
+    return <string>response.data.id_token;
+}
+
 export const saveDiagnosisKeys = (body: any) => {
     const {isInteroperabilityEnabled} = body;
     if (!isInteroperabilityEnabled) {
@@ -51,10 +79,8 @@ export const saveDiagnosisKeys = (body: any) => {
     db.collection(config.efgs.firestore.diagnosisKeysCollectionName)
         .doc(id)
         .set(itemToSave)
-        .catch(reason => {
-            throw new Error(reason)
-        });
+        .catch(reason => {throw new Error(reason)});
 };
 
 
-export default uploadDiagnosisKeysHttpHandler;
+export default uploadDiagnosisKeys;
