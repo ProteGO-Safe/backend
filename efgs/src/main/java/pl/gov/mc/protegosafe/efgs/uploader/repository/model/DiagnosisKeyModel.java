ca39bcf4d4beda534ae748473624f5a3ea913506
TomaszMolenda <tomasz.molenda@thecoders.io>
2020-11-12 23:16:33 +0100

    PSAFE-2799 - fix getter in model (#112)

--- efgs/src/main/java/pl/gov/mc/protegosafe/efgs/uploader/repository/model/DiagnosisKeyModel.java
+++ efgs/src/main/java/pl/gov/mc/protegosafe/efgs/uploader/repository/model/DiagnosisKeyModel.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class DiagnosisKeyModel {
+    private final Long createdAt = null;
     private final String key = null;
     private final Long rollingStartNumber = null;
     private final Integer rollingPeriod = null;
