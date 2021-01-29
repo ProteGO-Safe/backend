dbc8b129aaf8aba9634d5b6ed9e981c74ddc7d33
michalsikora1 <74990876+michalsikora1@users.noreply.github.com>
2021-01-29 08:34:39 +0100

    PSAFE-3426 - addd monitored resoource and tracing to cloud logger (#200)

--- efgs/uploader/src/main/java/pl/gov/mc/protegosafe/efgs/CloudEnhancer.java
+++ efgs/uploader/src/main/java/pl/gov/mc/protegosafe/efgs/CloudEnhancer.java
@@ -2,13 +2,18 @@ package pl.gov.mc.protegosafe.efgs;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import com.google.cloud.MonitoredResource;
 import com.google.cloud.logging.LogEntry;
 import com.google.cloud.logging.logback.LoggingEventEnhancer;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.UUID;
+
 import static java.util.Objects.nonNull;
 
 public class CloudEnhancer implements LoggingEventEnhancer {
+
+    private static final String executionId = UUID.randomUUID().toString();
 
     public void enhanceLogEntry(LogEntry.Builder builder, ILoggingEvent e) {
         e.getMDCPropertyMap().forEach((key, value) -> {
@@ -24,5 +29,14 @@ public class CloudEnhancer implements LoggingEventEnhancer {
         if (nonNull(e.getMarker()) && StringUtils.equals(e.getMarker().getName(), "CRITICAL")) {
             builder.addLabel("critical-stopcovid", e.getMessage());
         }
+
+        builder.addLabel("execution_id", executionId.substring(24));
+
+        final MonitoredResource resource = MonitoredResource
+                .newBuilder("cloud_function")
+                .addLabel("function_name", System.getenv("STACKDRIVER_LOG_NAME"))
+                .build();
+
+        builder.setResource(resource);
     }
 }
